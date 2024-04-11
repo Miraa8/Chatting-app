5a0aa1c81dedfc6b57569e3047f0750d935b1f68
amira qasim <104715002+Miraa8@users.noreply.github.com>
2024-04-11 15:25:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { connectDB } from "./DB/connection.js";
 import { Server } from "socket.io";
 const app = express();
 const port = 3000;
 
-connectDB();
 const server = app.listen(process.env.PORT || port, () => {
   console.log("app listening");
 });
